183a41d54aa39ea6316145e192dca1c2ad48bd71
Diego <diego.sanchez.et32@gmail.com>
2023-04-25 14:43:15 -0300

    componente input creado

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import { Route, Link, Switch } from "react-router-dom";
-
+import './App.css'
 import Login from './pages/Login';
 import Register from './pages/Register';
 import APP from './pages/APP';
@@ -9,12 +9,14 @@ import Settings from './pages/Settings';
 import About from './pages/About';
 import NotFound from './pages/NotFound';
 import Header from './components/Header';
+import Input from './components/utils/Input';
 
 
 function App() {
 
   return (
     <>
+    <div className='feo'>
       <Header/>
       <Switch>
         <Route exact path="/" component={About} />
@@ -25,7 +27,8 @@ function App() {
         <Route path="/about" component={About} />
         <Route component={NotFound} />
       </Switch>
-     
+      <Input type='text' value='gay' name='nombre' placeholder='pone tu nombre...' onChange={()=> console.log("Hola puto")}/>
+      </div>
     </>
   );
 }
