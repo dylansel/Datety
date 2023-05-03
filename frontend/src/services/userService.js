3eb6668726b34c32ab4f6cce25e38559094ba877
Dylan <91755493+DylanselET32@users.noreply.github.com>
2023-05-03 17:15:45 -0300

    Update userService.js

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -1,5 +1,24 @@
-var myHeaders = new Headers();
 
+
+async function addUser(user) {
+  try {
+    const response = await fetch(`${REACT_APP_API_URL}/user/addUser`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    });
+    const data = await response.json();
+    const status = await response.status();
+    return data,status
+  } catch (error) {
+    console.error(error);
+    throw new Error('Error al agregar usuario');
+  }
+}
+
+/*
 
 var raw = JSON.stringify({
   "name": "Dylan",
@@ -10,58 +29,4 @@ var raw = JSON.stringify({
   "photo": "dylan32.jpg"
 });
 
-var requestOptions = {
-  method: 'POST',
-  headers: myHeaders,
-  body: raw,
-  redirect: 'follow'
-};
-
-//-----------------------------------------
-
-var myHeaders = new Headers();
-
-var myInit = { method: 'GET',
-               headers: myHeaders,
-               mode: 'cors',
-               cache: 'default' };
-
-var myRequest = new Request('flowers.jpg', myInit);
-
-fetch(myRequest)
-.then(function(response) {
-  return response.blob();
-})
-.then(function(myBlob) {
-  var objectURL = URL.createObjectURL(myBlob);
-  myImage.src = objectURL;
-});
-
-//-----------------------------------------
-
-fetch(`${REACT_APP_API_URL}/user/addUser`)
-.then(function(response) {
-  if(response.ok) {
-    response.blob().then(function(miBlob) {
-      var objectURL = URL.createObjectURL(miBlob);
-      miImagen.src = objectURL;
-    });
-  } else {
-    console.log('Respuesta de red OK pero respuesta HTTP no OK');
-  }
-})
-.catch(function(error) {
-  console.log('Hubo un problema con la petición Fetch:' + error.message);
-});
-
-//-----------------------------------------
-
-
-
-
-
-
-fetch(`${REACT_APP_API_URL}/user/addUser`, requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
+*/
