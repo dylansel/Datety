9c44579870fe5945123316cd0ea5bb3d76b80945
GonzaGimenez <emaildegon@gmail.com>
2023-05-09 15:37:21 -0300

    Modal Aviso aplicado en APP

---
+++ frontend/src/components/ModalAviso.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+const ModalAvisoStyle = {
+  background: "#E7E6F7",
+  width: "25%",
+  height: "3rem",
+  borderRadius: "0.7rem",
+  margin: "0 auto",
+}
+
+export default function ModalAviso() {
+  return (
+    <>
+      <div className="modal-aviso-style" style={ModalAvisoStyle}>
+      </div>
+  </>
+  )
+}
--- frontend/src/pages/APP.jsx
+++ frontend/src/pages/APP.jsx
@@ -1,8 +1,24 @@
 import React, { useState, useEffect } from "react"
 import Header from "../components/utils/Header";
+import ModalAviso from "../components/ModalAviso"
 
 const isLoged = true;
 
+const ContenedorBotones = {
+  display: "flex",
+  justifyContent: "center",
+  padding: "40px",
+}
+
+const StyleButtonAviso = {
+  display: "flex",
+  padding: "10px 30px",
+  borderRadius: "100px",
+  color: "#fff",
+  border: "none",
+  background: "#0197F6",
+  cursor: "pointer",
+}
 
 export default function APP() {
   return (
@@ -10,6 +26,12 @@ export default function APP() {
       <Header isLoged={isLoged} />
       <div>
         <h2>APP</h2>
+      </div>
+      <div style={ContenedorBotones}>
+      <button style={StyleButtonAviso}>Aviso 1</button>
+      </div>
+      <div>
+      <ModalAviso msg="Gonza es un capo, crack de la vida"></ModalAviso>
       </div>
     </>
   )
