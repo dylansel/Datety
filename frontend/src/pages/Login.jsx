ff965327e3841504c97143e2838ab98cbf9e9e00
DylanselET32 <dylan.seltzer.et32@gmail.com>
2023-05-10 19:00:29 -0300

    addUser and editUser working and testing

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,27 +1,89 @@
 import React, { useState, useEffect } from "react"
 import Header from "../components/utils/Header"
-import { addUser } from "../services/userService";
+import { addUser,editUser } from "../services/UserService";
 
 const isLoged= false;
 
 export default function Login() {
   const user = {
-      name: "Dylan",
-      surname: "Seltzer",
-      email: "dylana.seltzer.et32@gmail.com",
-      userName: "dylansel32a",
-      password: "dylan32",
-      photo: "dylan32.jpg"
+      name: "Gonzalo",
+      surname: "Sanchez",
+      email: "Gonzalo.sanchezz.et32@gmail.com",
+      userName: "GonzaloS",
+      password: "GonzaloS",
+      photo: "GonzaloS.jpg"
   }
+
+  const userEdit = {
+    name: "diego",
+    photo: "diegooo.jpg"
+}
   const añadir = async () =>{
-    const result = await addUser(user)
-    console.log(result)
+    try {
+      const [result,status] = await addUser(user)
+      if(status == 200){
+       console.log("se creo correctamente")
+      }else if(result.message){
+        console.warn(`API CODE Warn: "${result.message}"`)
+      }else{
+        console.error(`API CODE Warn: "${result.error}"`)
+      }
+    } catch (error) {
+      console.log(error)
+    }
   }
+
+  const modificar = async () => {
+    try {
+      const [result,status] = await editUser(userEdit)
+      if(status == 200){
+       console.log("se modifico correctamente")
+      }else{
+        console.error(`API CODE ERR: "${result.message}"`)
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  const leer = async () => {
+    try {
+      const [result,status] = await getUser()
+      if(status == 200){
+       console.log("se creo correctamente")
+      }else{
+        console.error(`API CODE ERR: "${result.message}"`)
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  const eliminar = async () => {
+    try {
+      const [result,status] = await deleteUser()
+      if(status == 200){
+       console.log("se creo correctamente")
+      }else{
+        console.error(`API CODE ERR: "${result.message}"`)
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+
+
+
   return (
     <>
     <Header isLoged={isLoged} />
       <div>
-        <button onClick={añadir}>ENVIAR</button>
+        <button onClick={añadir}>Agregar</button>
+        <button onClick={modificar}>Modificar</button>
+        <button onClick={leer}>Leer</button>
+        <button onClick={eliminar}>Eliminar</button>
+        {/*ACA VA A MOSTRAR CUANDO TOQUE LEER*/}
       </div>
   </>
   )
--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -1,9 +1,8 @@
+import { setAuthToken,getAuthToken} from '../services/authService'
+ const apiUrl = import.meta.env.VITE_API_URL;
 
- 
  export async function addUser(user) {
   try {
-    const apiUrl = import.meta.env.VITE_API_URL;
-    console.warn(apiUrl)
     const response = await fetch(`${apiUrl}/user/addUser`, {
       method: 'POST',
       headers: {
@@ -12,14 +11,33 @@
       body: JSON.stringify(user)
     });
     const data = await response.json();
-    const status = await response.status();
-    return data,status
+    const status = response.status;
+    if(status === 200 && data.token){setAuthToken(data.token)}
+    return [data,status]
   } catch (error) {
     console.error(error);
     throw new Error('Error al agregar usuario');
   }
 }
 
+export async function editUser(user) {
+  try {
+    const response = await fetch(`${apiUrl}/user/editUser`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${getAuthToken()}`,
+      },
+      body: JSON.stringify(user)
+    });
+    const data = await response.json();
+    const status = response.status;
+    return [data,status]
+  } catch (error) {
+    console.error(error);
+    throw new Error('Error al agregar usuario');
+  }
+}
 
 
 
