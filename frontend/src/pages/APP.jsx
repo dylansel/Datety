79ab36e96bc5fe0e55ae5480c0393bb48206832c
Diego Sánchez Delgado <126181127+diegosanchezd@users.noreply.github.com>
2023-10-18 15:47:00 -0300

    calendar styles changed

--- frontend/src/pages/APP.jsx
+++ frontend/src/pages/APP.jsx
@@ -12,6 +12,8 @@ import ModalCreacion from "../components/ModalCreacion";
 import { useNavigate } from "react-router-dom";
 import Loading from "../components/misc/Loading";
 import { Dropdown, DropdownButton } from 'react-bootstrap';
+import "../stylesheets/calendar.css"
+
 const isLoged = true;
 
 export default  function APP({auth}) { 
@@ -66,10 +68,8 @@ export default  function APP({auth}) {
  
     const calendarStyle = {
       width:"100%",
-      margin:"auto",
-      height:"130rem",
-      position:"absolute",
       boxSizing: "border-box",
+      // background: "red"
     }
 
     const styleItemDropdown = {
@@ -108,8 +108,6 @@ export default  function APP({auth}) {
   
 
 
- 
-
   return (
     <>
     <div className={"app"}>
@@ -122,17 +120,23 @@ export default  function APP({auth}) {
           <Dropdown.Item   style={styleItemDropdown} >Evento Dinamico</Dropdown.Item>
         </DropdownButton>
         {creacion && <ModalCreacion handleModalCreacion={handleModalCreacion} isDinamic={false}/>}
-          <h2>APP</h2>
+          {/* <h2>APP</h2> */}
 
           {/* <button onClick={openModal}>Crear</button> */}
-          <button onClick={getAllEvents}>Ver todos Los Eventos</button>
-          <button onClick={()=>console.log(events)}>EVENTOS</button>
+          {/* <button onClick={getAllEvents}>Ver todos Los Eventos</button>
+          <button onClick={()=>console.log(events)}>EVENTOS</button> */}
 
           <div style={calendarStyle}>
-                <FullCalendar
+            <FullCalendar
                 
               timeZone="local"
             
+              height={"75vh"}
+              views= {{
+                timeGridMonth: {
+                dayMaxEventRows: 2 // ajustar a 6 solo para timeGridWeek/timeGridDay
+                }
+            }}
               eventChange={function (e){
                 console.log(e.event._def.title)
                 e.title= e.event._def.title
@@ -141,9 +145,10 @@ export default  function APP({auth}) {
               
               dateClick={function(info){
                 console.log('Clicked on: ' + info.dateStr);
-                console.log('Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY);
+                // console.log('Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY);
                 console.log('Current view: ' + info.view.type);
               }}
+
               locale= 'es'
               plugins={[dayGridPlugin, timeGridPlugin,interactionPlugin]}
               initialView="timeGridWeek"
@@ -153,9 +158,6 @@ export default  function APP({auth}) {
                 right: 'dayGridMonth,timeGridWeek,timeGridDay'
               }}
               
-              // eventChange={(e)=>{
-              //   console.log("CAMBIO: ", e._def.title)
-              // }}
               eventDrop={eventDrop}
               editable={true}
               selectable={false}
@@ -163,10 +165,8 @@ export default  function APP({auth}) {
               allDaySlot={false}
 
             />
+
           </div>
-          
-
-
         </div>
     </div>
 </>    
