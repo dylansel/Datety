b4751c76a86ef48f011fee56fe4b54cca6907532
GonzaGimenez <91755280+GonzaGimenez@users.noreply.github.com>
2023-10-18 17:03:01 -0300

    Update Settings.jsx

--- frontend/src/pages/Settings.jsx
+++ frontend/src/pages/Settings.jsx
@@ -1,8 +1,51 @@
 import React,{useState, useEffect} from "react"
 import Header from "../components/utils/Header"
 import { useNavigate } from "react-router-dom";
+import Input from "../components/utils/Input";
 
 const isLoged = true;
+
+const mainContainerStyle = {
+  display: "flex",
+  justifyContent: "space-evenly",
+  height: "100vh",
+  // background: "blue"
+}
+
+const userSection = {
+  margin: "5rem",
+  display: "flex",
+  flexDirection: "column",
+  width: "35%",
+  // background: "red"
+}
+
+const accountSection = {
+  margin: "5rem",
+  flexDirection: "column",
+  width: "35%",
+  // background: "red"
+  
+}
+
+const formSectionOne = {
+  display: "flex",
+  flexDirection: "column",
+  marginTop: "10px",
+  height: "30%",
+  // background: "green",
+  justifyContent: "space-between"
+}
+
+const formSectionTwo = {
+  display: "flex",
+  flexDirection: "column",
+  marginTop: "10px",
+  // background: "yellow",
+  height: "20%",
+  justifyContent: "space-evenly"
+
+}
 
 export default function Setting({auth}){
   //-----------------AUTHENTHICATION------------
@@ -22,9 +65,26 @@ export default function Setting({auth}){
 
   return(
     <>
-      <div>
-        <div>
-          <h2>SETTINGS </h2>
+      <div style={mainContainerStyle}>
+        <div style={userSection}>
+          <h2>User Settings</h2>
+          <form style={formSectionOne}>
+            <label>Cambiar nombre de usuario</label>
+            <Input/>
+            <label>Cambiar contraseña</label>
+            <Input/>
+            <input type="submit" value="Guardar cambios"/>
+            <input type="submit" value="Eliminar cuenta"/>
+
+          </form>
+        </div>
+        <div style={accountSection}>
+          <h2>Account Preferences Setting</h2>
+          <form style={formSectionTwo}>
+            <label>Comienzo de semana</label>
+            <Input/>
+            <label>Sleep time <input/> <span>to</span> <input/></label>
+          </form>
         </div>
       </div>
     </>
