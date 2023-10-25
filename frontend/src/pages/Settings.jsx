882df651059df5e261a11a6dd88f8eaf3e250ed3
Gonzalo Gimenez <emaildegon@gmail.com>
2023-10-25 17:31:45 -0300

    Settings functionallity

--- frontend/src/pages/Settings.jsx
+++ frontend/src/pages/Settings.jsx
@@ -2,10 +2,11 @@ import React,{useState, useEffect} from "react"
 import Header from "../components/utils/Header"
 import "../stylesheets/settings.css"
 import { useAlert } from "../contexts/AlertContext"
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Input from "../components/utils/Input";
 import { editUser, getUser, deleteUser } from "../services/userService";
-import { compareObjs } from "@fullcalendar/core/internal";
+import { getSettingsById, editSetting } from "../services/settingService";
+import { compareObjects } from "../helpers/misc/objectsUtils";
 
 const isLoged = true;
 
@@ -73,6 +74,9 @@ export default function Setting({auth}){
   const initialFilds = {
     userName: '',
     password: '',
+    startSleep: '',
+    endSleep: '',
+    darkTheme: null,
   }
 
   const [errorMsg, setErrorMsg] = useState();
@@ -80,6 +84,7 @@ export default function Setting({auth}){
   const { alertConfig, setAlertConfig } = useAlert();
   const [fetchData, setFetchData] = useState();
   const [userToEdit, setUserToEdit] = useState(initialFilds);
+  const [settingsToEdit, setSettingsToEdit] = useState(initialFilds);
   const [loaded, setLoaded] = useState(false);
   const [isSaving, setIsSaving] = useState(false);
 
@@ -87,20 +92,23 @@ export default function Setting({auth}){
     setErrorMsg(null);
     setFetchData(null);
     setUserToEdit(initialFilds);
+    setSettingsToEdit(initialFilds);
     setLoaded(false);
     setIsSaving(false);
     navigate(-1)
   }
 
   const fetch = async () => {
-    if (!auth?.user) {return}
+    if (!auth?.user) {
+      return;
+    }
     try {
       setLoaded(false);
       const profile = await getUser();
       if (profile.status != 200) {
         throw new Error(profile.data.message || profile.data.error)
       }
-      setFetchData({profile:profile.data})
+      setFetchData({ profile:profile.data })
       setUserToEdit(profile.data);
       setLoaded(true)
     } catch (error) {
@@ -109,14 +117,32 @@ export default function Setting({auth}){
         show: true,
         status: 'danger',
         title: 'Error',
-        message: `Hubo un error al traer los datos ${error.message}`
+        message: `Hubo un error al traer los datos: ${error.message}`
+      });
+    }
+    try {
+      setLoaded(false);
+      const settings = await getSettingsById(id );
+      if (settings.status !== 200) {
+        throw new Error(settings.data.message || settings.data.error)
+      }
+      setFetchData({ settings:settings.data })
+      setSettingsToEdit(settings.data);
+      setLoaded(true);
+    } catch (error) {
+      console.error(error);
+      setAlertConfig({
+        show: true,
+        status: 'danger',
+        title: 'Error',
+        message: `Hubo un error al traer los datos: ${error.message}`
       });
     }
 
   }
 
   const isChanged = () =>{
-    const userEdited = compareObjs(fetchData?.profile, userToEdit);
+    const userEdited = compareObjects(fetchData?.profile, userToEdit);
     return Object.keys(userEdited).length > 0;
   }
 
@@ -135,7 +161,8 @@ export default function Setting({auth}){
     e.preventDefault();
     setIsSaving(true);
     try {
-      const userEdited = compareObjs(fetchData?.profile, userToEdit);
+      const userEdited = compareObjects(fetchData?.profile, userToEdit);
+      console.log(userEdited)
       if (isChanged()) {
         const save = await editUser(userEdited, fetchData.profile.id);
         if (save.status == 200) {
@@ -163,6 +190,34 @@ export default function Setting({auth}){
     setCheck(!check);
   }
 
+  const handleAccountPreferencesSubmit = async () => {
+    e.preventDefault();
+    setIsSaving(true);
+    try {
+      const userEdited = compareObjects(fetchData?.profile, userToEdit);
+      if (isChanged()) {
+        const save = await editUser(userEdited, fetchData.profile.id);
+        if (save.status == 200) {
+          setAlertConfig({
+            show: true,
+            status: 'success',
+            title: 'Guardado',
+            message: `Se han guardado los cambios`
+          });
+          refresh();
+          handleClose();
+        } else {
+          throw new Error('ErrBackend', save.data?.error || save.data?.message);
+        }
+      }
+    } catch (error) {
+      console.error(error);
+      setErrorMsg(`Error al guardar, ${error.message}`)
+    } finally {
+      setIsSaving(false);
+    }
+  }
+
   useEffect(() => {
     fetch();
   }, []);
@@ -172,27 +227,25 @@ export default function Setting({auth}){
       <div style={mainContainerStyle}>
         <div style={userSection}>
           <h2>User Settings</h2>
-          <form style={formSectionOne} onSubmit={handleSubmit}>
+          <form style={formSectionOne} >
             <label>Cambiar nombre de usuario</label>
-            <Input name='name' type="text" value={userToEdit.userName} onChange={handleChange}/>
+            <Input name='userName' type="text" value={userToEdit.userName} onChange={handleChange}/>
             <label>Cambiar contraseña</label>
             <Input placeholder="Ingrese una nueva contraseña" name='password' value={userToEdit.password} type={check ? "text" : "password"} onChange={handleChange}/>
             <div>
                 <label htmlFor="mostrar_pass">Mostrar Contraseña</label> <input name="mostrar_pass" type="checkbox" onChange={handleCheck}/>
               </div>
             <div style={buttonsStyle}>
-              <input className="inputSubmit" type="submit" value="Guardar cambios"/>
-              <input className="inputDanger" type="submit" value="Eliminar cuenta"/>
+              <input className="inputSubmit" type="submit" value="Guardar cambios"  onClick={handleSubmit} />
+              <input className="inputDanger" type="button" value="Eliminar cuenta"/>
             </div>
 
           </form>
         </div>
         <div style={accountSection}>
           <h2>Account Preferences Setting</h2>
-          <form style={formSectionTwo} onSubmit={handleSubmit}>
-            <label>Comienzo de semana</label>
-            <Input onChange={handleChange}/>
-            <label>Sleep time <input type="time" onChange={handleChange}/> <span>to</span> <input type="time" onChange={handleChange}/></label>
+          <form style={formSectionTwo} onSubmit={handleAccountPreferencesSubmit}>
+            <label>Hora de inicio de sueño <input name="startSleep" type="time" value={userToEdit.startSleep} onChange={handleChange} /> <span>a</span> <input name="endSleep" type="time" value={userToEdit.endSleep} onChange={handleChange} /></label>
           </form>
         </div>
       </div>
